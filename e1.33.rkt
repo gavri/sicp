9546f3ddc40d32a75a7296e2d548e5bc9a75b5eb
Gavri Fernandez <gavri.fernandez@gmail.com>
2013-08-01 01:55:01 +0530

    a and b of e1.33

--- e1.33.rkt
+++ e1.33.rkt
@@ -1,4 +1,22 @@
 #lang racket
+(define (square n) (* n n))
+
+(define (inc n) (+ n 1))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define (next-with-filter next predicate a)
   (let ([next-a (next a)])
     (cond 
@@ -11,6 +29,7 @@
 (define (filtered-accumulate term a next b f init predicate)
   (accumulate-iter term a next b f init predicate)
   )
+
 (define (accumulate-iter term a next b f acc predicate)
   (if (> a b)
     acc 
@@ -22,4 +41,19 @@
             ]
           )
     )
+  )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (sum-of-squares-of-prime-numbers a b)
+  (filtered-accumulate square a inc b + 0 prime?)
+  )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (gcd a b)
+  (if (= b 0)
+    a
+    (gcd b (remainder a b))))
+(define (product-of-smaller-positive-integers-relatively-prime n)
+  (define (relatively-prime-to-n x)
+    (= 1 (gcd n x))
+    )
+  (filtered-accumulate identity 1 inc n * 1 relatively-prime-to-n)
   )
