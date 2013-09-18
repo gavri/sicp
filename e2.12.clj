d2080b5fa8c1106b8241dc7992cba53e2a214dc3
Gavri Fernandez <gavri.fernandez@gmail.com>
2013-09-18 20:24:44 +0530

    e2.12 in clojure

---
+++ e2.12.clj
@@ -0,0 +1,13 @@
+(ns e2.11 (:use clojure.test))
+(defn make-interval [x y] [x y])
+(defn make-center-percent [c p] (make-interval (- c (* p c)) (+ c (* p c))))
+(def lower-bound first)
+(def upper-bound second)
+(defn width [i] (- (upper-bound i) (lower-bound i)))
+(defn center [i] (/ (+ (lower-bound i) (upper-bound i)) 2))
+(defn percent [i] (/ (/ (width i) 2) (center i)))
+
+(deftest percent-test (is (= (/ 1 9) (percent (make-interval 4 5)))))
+(deftest make-center-percent-test (is (= (make-interval 3 5) (make-center-percent 4 (/ 1 4)))))
+
+(run-all-tests #"e2.12")
