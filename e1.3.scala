388f8c5465c5cada7227181ecc55e3bfbf74e3c2
gavri <gfernandez@rednovalabs.com>
2017-04-26 18:04:55 -0500

    e1.3: sqr -> square

--- e1.3.scala
+++ e1.3.scala
@@ -1,4 +1,4 @@
-def sqr(x: Int) = x * x
+def square(x: Int) = x * x
 def inverse(x: Int) = -x
 def twoLargest(l: List[Int]) = l.sortBy(inverse) take 2
-def sumOfSquaresOfTwoLargest(l: List[Int]) = twoLargest(l).map(sqr).sum
+def sumOfSquaresOfTwoLargest(l: List[Int]) = twoLargest(l).map(square).sum
