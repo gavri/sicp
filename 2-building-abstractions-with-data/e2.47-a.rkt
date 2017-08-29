7b117c7c9fd44e13d7c4ea40fd7c807337401d9e
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-08-29 15:37:07 +0700

    2.47

---
+++ 2-building-abstractions-with-data/e2.47-a.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+
+(define origin-frame car)
+(define edge1-frame cadr)
+(define edge2-frame caddr)
---
+++ 2-building-abstractions-with-data/e2.47-b.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+
+(define origin-frame car)
+(define edge1-frame cadr)
+(define edge2-frame cddr)
