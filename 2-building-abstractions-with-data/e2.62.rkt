56b8370076860b043eed96329f106f496abbc41d
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-09-07 12:48:41 +0700

    2.62

---
+++ 2-building-abstractions-with-data/e2.62.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (let ((x1 (car set1)) (x2 (car set2)))
+                (cond ((= x1 x2)
+                       (cons x1
+                             (union-set (cdr set1)
+                                        (cdr set2))))
+                      ((< x1 x2)
+                       (cons x1 (union-set (cdr set1) set2)))
+                      ((< x2 x1)
+                       (cons x2 (union-set set1 (cdr set2)))))))))
+
+(require rackunit)
+
+(check-equal? (union-set '(1 2 3) '(0 2 4)) '(0 1 2 3 4))
