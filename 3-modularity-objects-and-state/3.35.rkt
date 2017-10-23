7307c60fbc7e11d81e668faf9641d472836834a2
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-10-23 23:14:07 +0700

    3.35

---
+++ 3-modularity-objects-and-state/3.35.rkt
@@ -0,0 +1,115 @@
+#lang racket
+
+(define (inform-about-value constraint)
+  (constraint 'I-have-a-value))
+(define (inform-about-no-value constraint)
+  (constraint 'I-lost-my-value))
+
+(define (for-each-except exception procedure list)
+  (define (loop items)
+    (cond ((null? items) 'done)
+          ((eq? (car items) exception) (loop (cdr items)))
+          (else (procedure (car items))
+                (loop (cdr items)))))
+  (loop list))
+
+(define (connect connector new-constraint)
+  ((connector 'connect) new-constraint))
+(define (has-value? connector)
+  (connector 'has-value?))
+(define (get-value connector)
+  (connector 'value))
+(define (set-value! connector new-value informant)
+  ((connector 'set-value!) new-value informant))
+(define (forget-value! connector retractor)
+  ((connector 'forget) retractor))
+
+(define (make-connector)
+  (let ((value false) (informant false) (constraints '()))
+    (define (set-my-value newval setter)
+      (cond ((not (has-value? me))
+             (set! value newval)
+             (set! informant setter)
+             (for-each-except setter
+                              inform-about-value
+                              constraints))
+            ((not (= value newval))
+             (error "Contradiction" (list value newval)))
+            (else 'ignored)))
+    (define (forget-my-value retractor)
+      (if (eq? retractor informant)
+        (begin (set! informant false)
+               (for-each-except retractor
+                                inform-about-no-value
+                                constraints))
+        'ignored))
+
+    (define (connect new-constraint)
+      (if (not (memq new-constraint constraints))
+        (set! constraints
+          (cons new-constraint constraints)) null)
+      (if (has-value? me)
+        (inform-about-value new-constraint) null)
+      'done)
+    (define (me request)
+      (cond ((eq? request 'has-value?)
+             (if informant true false))
+            ((eq? request 'value) value)
+            ((eq? request 'set-value!) set-my-value)
+            ((eq? request 'forget) forget-my-value)
+            ((eq? request 'connect) connect)
+            (else (error "Unknown operation -- CONNECTOR"
+                         request))))
+    me))
+
+(define (squarer a b)
+  (define (process-new-value)
+    (if (has-value? b)
+      (if (< (get-value b) 0)
+        (error "square less than 0 -- SQUARER" (get-value b))
+        (set-value! a (sqrt (get-value b)) me))
+      (if (has-value? a)
+        (set-value! b (* (get-value a) (get-value a)) me)
+        'ignored)))
+  (define (process-forget-value)
+    (forget-value! a me)
+    (forget-value! b me)
+    (process-new-value)
+    )
+  (define (me request)
+    (cond ((eq? request 'I-have-a-value)
+           (process-new-value))
+          ((eq? request 'I-lost-my-value)
+           (process-forget-value))
+          (else
+            (error "Unknown request -- MULTIPLIER" request))))
+  (connect a me)
+  (connect b me)
+  me)
+
+(require rackunit)
+
+(define a (make-connector))
+(define b (make-connector))
+
+(squarer a b)
+
+(check-equal? (get-value a) #f)
+(check-equal? (get-value b) #f)
+
+(set-value! a 3 'user)
+
+(check-equal? (get-value b) 9)
+
+(forget-value! a 'user)
+
+(check-equal? (has-value? a) #f)
+(check-equal? (has-value? b) #f)
+
+(set-value! b 16 'user)
+
+(check-equal? (has-value? a) #t)
+(check-equal? (has-value? b) #t)
+
+(check-equal? (get-value a) 4)
+(check-equal? (get-value b) 16)
