c5d71e09b813c3c78bcc5fb0e5d01e1130a3c574
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-10-31 21:13:17 +0700

    3.48

---
+++ 3-modularity-objects-and-state/3.48.rkt
@@ -0,0 +1,63 @@
+#lang racket
+(require compatibility/mlist)
+
+; Just a stub. Non-atomic
+(define (test-and-set! cell)
+  (if (car cell)
+    true
+    (begin (set-mcar! cell true)
+           false)))
+
+(define (make-mutex)
+  (let ((cell (list false)))
+    (define (the-mutex m)
+      (cond ((eq? m 'acquire)
+             (if (test-and-set! cell)
+               (the-mutex 'acquire) 'ignored)) ; retry
+            ((eq? m 'release) (clear! cell))))
+    the-mutex))
+(define (clear! cell)
+  (set-mcar! cell false))
+
+(define (make-serializer)
+  (let ((mutex (make-mutex)))
+    (lambda (p)
+      (define (serialized-p . args)
+        (mutex 'acquire)
+        (let ((val (apply p args)))
+          (mutex 'release)
+          val))
+      serialized-p)))
+
+(define (make-account-and-serializer account-number balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin (set! balance (- balance amount))
+             balance)
+      "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((balance-serializer (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) withdraw)
+            ((eq? m 'deposit) deposit)
+            ((eq? m 'balance) balance)
+            ((eq? m 'serializer) balance-serializer)
+            ((eq? m 'account-number) account-number)
+            (else (error "Unknown request -- MAKE-ACCOUNT"
+                         m))))
+    dispatch))
+
+(define (exchange account1 account2)
+  (let ((difference (- (account1 'balance)
+                       (account2 'balance))))
+    ((account1 'withdraw) difference)))
+
+(define (serialized-exchange account1 account2)
+  (let ((serializer1 (account1 'serializer))
+        (serializer2 (account2 'serializer)))
+    (if (< (account1 'account-number) (account2 'account-number))
+      ((serializer1 (serializer2 exchange)) account1 account2)
+      ((serializer2 (serializer1 exchange)) account1 account2))
+    ))
