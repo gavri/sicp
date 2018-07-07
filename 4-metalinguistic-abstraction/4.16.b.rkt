3905fa2911332840adaabb85679f7a1725e27625
Gavri Fernandez <gavri.fernandez@gmail.com>
2018-07-07 10:51:43 +0530

    4.16.b: Support named procedures

--- 4-metalinguistic-abstraction/4.16.b.rkt
+++ 4-metalinguistic-abstraction/4.16.b.rkt
@@ -4,28 +4,32 @@
                                 ((eq? (car exp) 'define) true)
                                 (else false)))
 
-(define (params-from-proc proc) (cadr proc))
 (define (body proc) (cddr proc))
 (define (definitions-from-body body) (filter definition? body))
 (define (body-without-definitions body) (filter (compose not definition?) body))
 
-(define (scan-out-defines proc)
-  (let* ((body (body proc))
-        (definitions (definitions-from-body body))
-        (definition-vars (map cadr definitions))
-        (rest-of-body (body-without-definitions body)))
-        (list 'lambda (params-from-proc proc)
-              (cons 'let
-                    (cons
-                      (map (lambda (definition-var) (list definition-var ''*unassigned*)) definition-vars)
-                      (append
-                        (map (lambda (definition) (list 'set! (second definition) (third definition))) definitions)
-                        (body-without-definitions body)))))))
+(define (scan-out-defines-from-body body)
+  (let* ((definitions (definitions-from-body body))
+         (definition-vars (map cadr definitions))
+         (rest-of-body (body-without-definitions body)))
+    (cons 'let
+          (cons
+            (map (lambda (definition-var) (list definition-var ''*unassigned*)) definition-vars)
+            (append
+              (map (lambda (definition) (list 'set! (second definition) (third definition))) definitions)
+              (body-without-definitions body))))))
 
+
+(define (scan-out-defines proc)
+  (cons (car proc) (list (cadr proc) (scan-out-defines-from-body (cddr proc)))))
 
 (require rackunit)
 
-(define input '(lambda (vars) (define u e1) (define v e2) e3 (define w e4) e5))
-(define expected '(lambda (vars) (let ((u (quote *unassigned*)) (v (quote *unassigned*)) (w (quote *unassigned*))) (set! u e1) (set! v e2) (set! w e4) e3 e5)))
+(define anon-proc-input '(lambda (vars) (define u e1) (define v e2) e3 (define w e4) e5))
+(define anon-proc-expected '(lambda (vars) (let ((u (quote *unassigned*)) (v (quote *unassigned*)) (w (quote *unassigned*))) (set! u e1) (set! v e2) (set! w e4) e3 e5)))
 
-(check-equal? (scan-out-defines input) expected)
+(define named-proc-input '(define (vars) (define u e1) (define v e2) e3 (define w e4) e5))
+(define named-proc-expected '(define (vars) (let ((u (quote *unassigned*)) (v (quote *unassigned*)) (w (quote *unassigned*))) (set! u e1) (set! v e2) (set! w e4) e3 e5)))
+
+(check-equal? (scan-out-defines anon-proc-input) anon-proc-expected)
+(check-equal? (scan-out-defines named-proc-input) named-proc-expected)
